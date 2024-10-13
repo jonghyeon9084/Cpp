7d261ff3c753edc6f5fb9658e8a28b35d2a67099
jonghyeon9084 <jonghyeon9084@gmail.com>
2024-10-13 22:38:45 +0900

    Update main(1번).cpp

--- HW/hw#3 - (이종현, 최원기)/main(1번).cpp
+++ HW/hw#3 - (이종현, 최원기)/main(1번).cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+using namespace std;
 
+class Dept {
+	int size;		
+	int* scores;	
+
+public:
+	Dept(int size) {				
+		this->size = size;
+		scores = new int[size];
+	}
+
+
+	Dept(const Dept& dept) {
+		size = dept.size;
+		scores = new int[size];
+		for (int i = 0; i < size; i++) {
+			scores[i] = dept.scores[i];
+		}
+	}
+
+	
+	~Dept() {
+		delete[] scores;
+	}
+
+	int getSize() { return size; }
+
+	void read() {
+		cout << "10개 점수 입력>> ";
+		for (int i = 0; i < size; i++) {
+			cin >> scores[i];
+		}
+	}
+
+	bool isOver60(int index) {
+		return scores[index] >= 60;
+	}
+};
+
+int countPass(Dept dept) {			
+	int count = 0;
+	for (int i = 0; i < dept.getSize(); i++) {
+		if (dept.isOver60(i)) count++;
+	}
+	return count;
+}
+
+int main() {
+	Dept com(10);				
+	com.read();					
+	int n = countPass(com);		
+	cout << "60점 이상은 " << n << "명";
+}
